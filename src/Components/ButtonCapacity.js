8036b28a56253117427c60d762a9d182a0ac13f5
azerty <audrene.coatmeur@gmail.com>
2020-08-12 16:54:41 +0200

    working on spread operator

--- src/Components/ButtonCapacity.js
+++ src/Components/ButtonCapacity.js
@@ -1,21 +1,21 @@
 import React from 'react';
+import { useDispatch } from "react-redux";
 
+const ButtonCapacity = () => {
 
-
-class ButtonCapacity extends React.Component {
-     combat = () => {
-        console.log('aie !')
+    const dispatch = useDispatch()
+    const hitMonster = () => {
+        dispatch({type: 'hitMonster', payload: {damages: 10}}) 
+        dispatch({type: 'hitPlayer', payload: {damages: 15}})
     }
-    render() {
-        return (
-            <button type="button" onClick={() => this.combat()} className="btn btn-success material-tooltip-main ">
-                hit
+    
+    return (
+        <button type="button" onClick={() => hitMonster()} className="btn btn-success material-tooltip-main ">
+            hit
             <i className="fas fa-bomb"></i> 5
-        <i className="fas fa-fire-alt"></i> - 5
-    </button>
-        )
-    }
-
+            <i className="fas fa-fire-alt"></i> - 5
+        </button>
+    )
 }
 
 
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,12 +1,48 @@
 const initialState = {
-    //TODO : complete players {} and monster{}
-    players: {},
-    monster: {}
+    players: {
+      1: { name: "John", pv: 25, pvMax: 110, mana: 15, manaMax: 40, id: 1 },
+      2: { name: "Jack", pv: 110, pvMax: 120, mana: 40, manaMax: 50, id: 2 },
+      3: { name: "Jessy", pv: 80, pvMax: 130, mana: 25, manaMax: 60, id: 3 },
+      4: { name: "Jenny", pv: 130, pvMax: 140, mana: 0, manaMax: 70, id: 4 }
+    },
+    monster: {
+      name: "Hamza",
+      pv: 450,
+      pvMax: 500
+    }
   };
    
-  function rootReducer(state = initialState, action) {
-    return state;
-  };
+  const rootReducer = (state = initialState, action) => {
+
+    if (action.type === 'hitMonster') {
+      let newState = {
+        ...state,
+        monster: {
+          ...state.monster,
+          pv: state.monster.pv - action.payload.damages
+        }
+      }
+      console.log(newState);
+      return newState
+    }
+
+    if (action.type === 'hitPlayer') {
+      let newState = {
+        ...state,
+        players: {
+          ...state.players,
+          // 1: {
+          //   ...state.players{1},
+          //   pv: state.players{1} - action.payload.damages
+          // }
+        }
+      }
+      console.log(newState);
+      return newState
+    }
+
+      return state;
+  }
    
   export default rootReducer;
   
